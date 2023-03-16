f2b65b8f487109c9a6acafe592fd68c9d7d9cb2c
nishan <nishangautam.ng@gmail.com>
2023-03-16 12:50:06 +1100

    some more changes

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ async function getWeather(cityName) {
   try {
     // Use the fetch API to make a GET request to the OpenWeatherMap API using the apiUrl
     const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
     const data = await response.json();
     // Get references to the relevant DOM elements
     const city = document.getElementById("city");
     const temperature = document.getElementById("temperature");
     const description = document.getElementById("description");
-    city.textContent = cityName;
+    city.textContent = data.name;
     // Set the text content of the temperature element to the temperature property of the data object, rounded to one decimal place
     temperature.textContent = `${Math.round(data.main.temp * 10) / 10}°C`;
     // Set the text content of the description element to the description property of the data object
